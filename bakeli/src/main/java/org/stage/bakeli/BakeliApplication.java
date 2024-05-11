498203f09cc168ed9ab8ef699d67a74540760a32
mouhamedhanne <mouhamedhanne72@gmail.com>
2024-05-11 00:03:15 +0000

    Initialisation

---
+++ bakeli/src/main/java/org/stage/bakeli/BakeliApplication.java
@@ -0,0 +1,13 @@
+package org.stage.bakeli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakeliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakeliApplication.class, args);
+	}
+
+}
---
+++ bakeli/src/test/java/org/stage/bakeli/BakeliApplicationTests.java
@@ -0,0 +1,13 @@
+package org.stage.bakeli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakeliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
